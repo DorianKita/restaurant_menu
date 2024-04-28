c7f2b49a66fb28e405fd799edca9ae0828c0f383
Dorian Kita <dorian.kita90@gmail.com>
2024-04-28 10:24:34 +0200

    cleared whole project to start over again

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import Cart from "./components/Cart.jsx";
-import Checkout from "./components/Checkout.jsx";
-import Header from "./components/Header.jsx";
-import Meals from "./components/Meals.jsx";
-import { CartContextProvider } from "./store/CartContext.jsx";
-import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
-
 function App() {
-  return (
-    <UserProgressContextProvider>
-      <CartContextProvider>
-        <Header />
-        <Meals />
-        <Cart />
-        <Checkout />
-      </CartContextProvider>
-    </UserProgressContextProvider>
-  );
+  return <h1>hello</h1>;
 }
 
 export default App;
